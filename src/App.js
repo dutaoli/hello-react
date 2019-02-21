8a72f5a3e6ec838071705b7fbd74577a57d29ded
Torri du <torridu@ML00070077.local>
2019-02-21 14:08:35 +0800

    Life cycle function

--- src/App.js
+++ src/App.js
@@ -4,25 +4,62 @@ import Header from './components/Header';
 import Home from './components/Home'
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      headerLink: 'Header',
+      homeMounted: true
+    }
+  }
+  onGreet(age){
+    alert(age);
+  }
+
+  handleChangeLink(newLinkName){
+    this.setState({headerLink: newLinkName})
+  }
+
+  handleUnmount(){
+    this.setState({
+      homeMounted: !this.state.homeMounted
+    });
+  }
+  
   render() {
     const user = {
       name: "Anna",
       hobbies: ["Running", "Writting"]
     }
+    let homePage = "";
+    if(this.state.homeMounted) {
+      homePage = (
+        <Home 
+            name={"max"} 
+            age={12} 
+            user={user} 
+            greet={this.onGreet} 
+            headerLink={this.handleChangeLink.bind(this)}
+            initialName={this.state.headerLink}
+            handleUnmount={this.handleUnmount.bind(this)}>
+          <p>son element</p>
+        </Home>
+      );
+    }
     return (
       <div className="container">
-        <div className="row">
+      <div className="row">
           <div className="col-xs-1 col-xs-offset-11">
-          <Home name={"max"} age={12} user={user}>
-            <p>son element</p>
-          </Home>
+          <Header linkName={this.state.headerLink} />
           </div>
         </div>
+
         <div className="row">
           <div className="col-xs-1 col-xs-offset-11">
-          <Header />
+            {homePage}
           </div>
         </div>
+        <hr />
+        <button onClick={this.handleUnmount.bind(this)}>(Un)mount the component</button>
         <div className="row">
           <div className="col-xs-1 col-xs-offset-11">
           <h1>Hello World!</h1>
--- src/components/Home.js
+++ src/components/Home.js
@@ -6,17 +6,54 @@ export default class Home extends Component {
   constructor(props){
     super(props);
     this.state = {
-      age: props.age
+      age: props.age,
+      headerLink: props.initialName
     }
+    console.log('constructor');
   }
 
   onMakeOlder() {
     this.setState({
       age: this.state.age + 3
     })
-    
+
     console.log(this);
   }
+
+  handleGreet() {
+    this.props.greet(this.state.age);
+  }
+  handleChangeLink() {
+    this.props.headerLink(this.state.headerLink)
+  }
+  handleChangeName(e) {
+    this.props.headerLink(e.target.value);
+  }
+  componentWillMount() {
+    console.log("component will mount");
+  }
+  componentDidMount() {
+    console.log("component did mount");
+  }
+
+  componentWillReceiveProps(nextProps) {
+    console.log("component will receive props", nextProps);
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("component should update", nextProps, nextState);
+    return false;
+  }
+  componentWillUpdate(nextProps, nextState) {
+    console.log("component will update", nextProps, nextState);
+  }
+  componentDidUpdate(prevProps, prevState) {
+    console.log("component did update", prevProps, prevState);
+  }
+
+  componentWillUnmount() {
+    console.log("component will unmount");
+  }
+
   render() {
     console.log(this.props);
 
@@ -33,6 +70,11 @@ export default class Home extends Component {
           </div>
           <button className="btn btn-primary" onClick={this.onMakeOlder.bind(this)}>Add age</button>
           <button className="btn btn-primary" onClick={() => {this.onMakeOlder()}}>Make me older</button>
+          <hr />
+          <button onClick={this.handleGreet.bind(this)}>Greet</button>
+          <br />
+          <input defaultValue={this.props.initialName} onChange={(e) => this.handleChangeName(e)} />
+          <button onClick={this.handleChangeLink.bind(this)}>Change link</button>
         </div>
       </div>
     );
@@ -42,5 +84,8 @@ export default class Home extends Component {
 Home.propTypes = {
   name: PropTypes.string,
   age: PropTypes.number,
-  user: PropTypes.object
+  user: PropTypes.object,
+  children: PropTypes.element.isRequired,
+  greet: PropTypes.func,
+  initialName: PropTypes.string
 };
